bf91377d15cd869d13b58bb76346e0d701c722e6
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-02 11:22:28 -0700

    updated

--- my-app/src/components/Pages/HeroSection.jsx
+++ my-app/src/components/Pages/HeroSection.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
- const Main = () => {
-    return ( <>
-    
-    
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas laudantium nobis commodi odit quae repellendus nisi praesentium rem voluptatibus sapiente?</p>
-    
-    </> );
- }
-  
- export default Main;
+import React from "react";
+import "../Pages/Hero.css";
+import img1 from "./Assets/iPhone-13-Pro-Front.png";
+import img2 from "./Assets/iPhone-13-Pro-Fronts.png";
+const Main = () => {
+  return (
+    <>
+      <div className="parent">
+        <div className="child">
+          <h1 id="text">Make The Best </h1>
+          <h1>Finacial Decisions</h1>
+        </div>
+        <div className="child-2">
+          <img id="img1" src={img1} alt="img1" />
+          <img id="img2" src={img2} alt="img2" />
+          <img id="img3" src={img2} alt="img3" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
